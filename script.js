832f52a7209279bb9e883b10d2f32e2ef1691f24
Tirtha~ <tirthasasane07@gmail.com>
2025-12-19 11:24:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,106 +1,87 @@
-const canvas = document.getElementById("fireworks");
-const ctx = canvas.getContext("2d");
-const btn = document.getElementById("celebrateBtn");
-const letter = document.getElementById("letter");
+const btn = document.getElementById('startBtn');
+}
+update() {
+this.y -= this.speed;
+if (this.y <= this.targetY) {
+explode(this.x, this.y, this.color);
+return true;
+}
+return false;
+}
+draw() {
+ctx.beginPath();
+ctx.arc(this.x, this.y, 2.5, 0, Math.PI * 2);
+ctx.shadowBlur = 20;
+ctx.shadowColor = this.color;
+ctx.fillStyle = this.color;
+ctx.fill();
+}
+}
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-let rockets = [];
-let particles = [];
-let fireworksOn = false;
+class Particle {
+constructor(x, y, color) {
+this.x = x;
+this.y = y;
+this.radius = Math.random() * 2 + 1;
+this.color = color;
+this.speedX = (Math.random() - 0.5) * 6;
+this.speedY = (Math.random() - 0.5) * 6;
+this.life = 100;
+}
+update() {
+this.x += this.speedX;
+this.y += this.speedY;
+this.life--;
+}
+draw() {
+ctx.beginPath();
+ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+ctx.shadowBlur = 25;
+ctx.shadowColor = this.color;
+ctx.fillStyle = this.color;
+ctx.fill();
+}
+}
 
-/* CLICK CELEBRATE */
-btn.addEventListener("click", () => {
-  btn.remove();               // delete button
-  letter.classList.remove("hidden");
-  fireworksOn = true;
+
+function explode(x, y, color) {
+for (let i = 0; i < 60; i++) {
+particles.push(new Particle(x, y, color));
+}
+}
+
+
+function launchLoop() {
+setInterval(() => {
+fireworks.push(new Firework());
+}, 700);
+}
+
+
+function animate() {
+ctx.fillStyle = 'rgba(0, 0, 0, 0.2)';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+
+fireworks = fireworks.filter(fw => {
+fw.draw();
+return !fw.update();
 });
 
-/* ROCKET */
-class Rocket {
-  constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = canvas.height;
-    this.speed = 7 + Math.random() * 3;
-    this.targetY = canvas.height * (0.2 + Math.random() * 0.15);
-  }
 
-  update() {
-    this.y -= this.speed;
-    if (this.y <= this.targetY) {
-      this.explode();
-      return false;
-    }
-    return true;
-  }
+particles = particles.filter(p => {
+p.draw();
+p.update();
+return p.life > 0;
+});
 
-  draw() {
-    ctx.strokeStyle = "#FFD700";
-    ctx.lineWidth = 2.5;
-    ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x, this.y + 15);
-    ctx.stroke();
-  }
 
-  explode() {
-    for (let i = 0; i < 100; i++) {
-      particles.push(new Particle(this.x, this.y));
-    }
-  }
+requestAnimationFrame(animate);
 }
 
-/* PARTICLES */
-class Particle {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-    this.vx = (Math.random() - 0.5) * 7;
-    this.vy = (Math.random() - 0.5) * 7;
-    this.life = 80;
-  }
 
-  update() {
-    this.vy += 0.05;
-    this.x += this.vx;
-    this.y += this.vy;
-    this.life--;
-  }
-
-  draw() {
-    ctx.fillStyle = "rgba(255, 215, 0, 0.9)";
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
-    ctx.fill();
-  }
-}
-
-/* LOOP */
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-  if (fireworksOn && Math.random() < 0.05) {
-    rockets.push(new Rocket());
-  }
-
-  rockets = rockets.filter(r => {
-    r.draw();
-    return r.update();
-  });
-
-  particles = particles.filter(p => {
-    p.update();
-    p.draw();
-    return p.life > 0;
-  });
-
-  requestAnimationFrame(animate);
-}
-
-animate();
-
-window.addEventListener("resize", () => {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+window.addEventListener('resize', () => {
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 });
